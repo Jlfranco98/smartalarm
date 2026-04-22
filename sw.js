264300f9c077ba9d2b506081649fad1f87c96dba
Jlfranco98 <samsungjose1998@gmail.com>
2026-04-22 07:44:09 +0200

    Change cache version to 'mialarm-v5'
    Updated cache version to v5 in service worker.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mialarm-v4.9'; // <--- CADA VEZ QUE SUBAS ALGO, CAMBIA ESTO (v2, v3, v4...)
+const CACHE = 'mialarm-v5'; // <--- CADA VEZ QUE SUBAS ALGO, CAMBIA ESTO (v2, v3, v4...)
 const ASSETS = [
   '/',
   '/index.html',
